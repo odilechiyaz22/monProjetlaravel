c1af9fdd51f5fec6d9e3854d7214a40aab9f2815
ODILE <odilechiyaz2@gmail.com>
2021-04-21 08:00:08 +0200

    Configuration de la base des donnees

--- database/migrations/2021_04_20_150801_create_naissances_table.php
+++ database/migrations/2021_04_20_150801_create_naissances_table.php
@@ -14,7 +14,27 @@ class CreateNaissancesTable extends Migration
     public function up()
     {
         Schema::create('naissances', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->string('postnom');
+            $table->string('prenom');
+            $table->string('sexe');
+            $table->string('lieu');
+            $table->string('date');
+            $table->string('nom_pere');
+            $table->string('prenom_pere');  
+            $table->string('date_pere');
+            $table->string('lieu_pere');
+            $table->string('profession');
+            $table->string('domicile');
+            $table->string('nom_mere');
+            $table->string('prenom_mere');  
+            $table->string('date_mere');
+            $table->string('lieu_mere');
+            $table->string('profession_mere');
+            $table->string('domicile_mere');
+            $table->string('tiers_declarant'); 
+            $table->string('evenements_relatifs');          
             $table->timestamps();
         });
     }
